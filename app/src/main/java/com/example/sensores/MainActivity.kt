94571cc96153d7f0874196b2f8d18cce488e2074
ElNumeroXIII <99847577+ElNumeroXIII@users.noreply.github.com>
2025-02-03 10:07:57 +0100

    Update de modificaciones

--- app/src/main/java/com/example/sensores/MainActivity.kt
+++ app/src/main/java/com/example/sensores/MainActivity.kt
@@ -1,20 +1,62 @@
 package com.example.sensores
 
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.sensores.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sensorManager: SensorManager
+    private var acelerometro: Sensor? = null
+    private var giroscopio: Sensor? = null
+    private var sensorLuz: Sensor? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //Inicializamos Sensormanager
+        sensorManager = getSystemService(SENSOR_SERVICE)as SensorManager
+        iniciarSensores()
+        setListeners()
+
     }
+
+    private fun iniciarSensores() {
+        acelerometro = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        giroscopio = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        sensorLuz = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+
+    }
+
+    private fun setListeners() {
+        binding.btSalir.setOnClickListener {
+            finish()
+        }
+    }
+
+    // ESTO HAY QUE PONERLO DE DEFAULT SIEMPRE QUE METEMOS SENSORES
+
+    override fun onPause() {
+
+        super.onPause()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
 }
